4b11c144c03e340b2d0c11d7bc15e588318dd420
David Gaona <davidjonatan00@gmail.com>
2025-05-30 10:03:25 -0500

    controller

--- backend/src/product/product.controller.ts
+++ backend/src/product/product.controller.ts
@@ -1,13 +1,23 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Logger, InternalServerErrorException  } from '@nestjs/common';
 import { ProductService } from './product.service';
 import { Product } from './product.entity';
 
 @Controller('products')
 export class ProductController {
+  private readonly logger = new Logger(ProductController.name);
   constructor(private readonly productService: ProductService) {}
 
   @Get()
-  findAll(): Promise<Product[]> {
-    return this.productService.findAll();
+  async findAll(): Promise<Product[]> {
+    this.logger.log('Received request to fetch all products.');
+    try {
+      return await this.productService.findAll();
+    } catch (error) {
+      this.logger.error('Error in controller while fetching products:', error.message);
+      if (!(error instanceof InternalServerErrorException)) {
+          throw new InternalServerErrorException('An unexpected error occurred in the product controller.');
+      }
+      throw error;
+    }
   }
 }
