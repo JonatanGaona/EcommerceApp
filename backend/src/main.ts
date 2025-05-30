98e89de8f32a358f500fb9551ef90c90bc8f1c56
David Gaona <davidjonatan00@gmail.com>
2025-05-30 11:40:58 -0500

    main_console_prueba

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -18,7 +18,8 @@ async function bootstrap() {
   
   await app.listen(port, '0.0.0.0');
   console.log(`Application is running on: http://0.0.0.0:${port}`);
-  console.log("Fin nuevo main")
+  console.log("Fin nuevo main");
+  console.log("FIN MAIN.TS - DEPLOY PRUEBA - 11:XX AM");
 }
 
 bootstrap();
